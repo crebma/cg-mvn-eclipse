a857de19d40f691ac526ebc831e44a057cf0bfb8
Amber Conville <crebma@gmail.com>
2013-01-29 22:22:18 -0500

    eclipse maven android project

---
+++ src/test/java/com/example/cg_mvn_eclipse/MainActivityTest.java
@@ -0,0 +1,18 @@
+package com.example.cg_mvn_eclipse;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import com.xtremelabs.robolectric.RobolectricTestRunner;
+
+@RunWith(RobolectricTestRunner.class)
+public class MainActivityTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
